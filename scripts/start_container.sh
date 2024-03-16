e1d9bec5a3bb236098cbf149bd8f010d8b11816b
harisheoran <harisheoran01@gmail.com>
2024-03-16 16:43:56 +0530

    added appspec file

---
+++ scripts/start_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+################################
+# Author: Harish Sheoran
+# Purpose: To start the container
+# Date: March 16th, 2024
+# Version: v1
+################################
+
+set -e
+
+docker container run -d -p 4000:4000 --name view_tracker_container --env-file ./.env harisheoran/view_api_img
---
+++ scripts/stop_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+################################
+# Author: Harish Sheoran
+# Purpose: To stop the container
+# Date: March 16th, 2024
+# Version: v1
+################################
+
+set -e
+
+docker container rm -f view_tracker_container
